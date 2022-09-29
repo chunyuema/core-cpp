422d74d9b605dce485915f21ffac94b9a9cb4d4a
Chunyue Ma <cma01@wesleyan.edu>
2022-09-29 07:59:15 -0700

    add code for conditions and branching

---
+++ basic_syntax/conditions.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void condition_as_integer()
+{
+    int x = 7;
+    int y = 9;
+    printf("x > y is: %d\n", x > y);
+    printf("x < y is: %d\n", x < y);
+}
+
+void multi_switch()
+{
+    // to be implemented
+}
+
+int main()
+{
+    condition_as_integer();
+    // int v1 = 7;
+    // int v2 = 8;
+    // if (v1 > v2)
+    // {
+    //     cout << "v1 is larger than v2" << endl;
+    // }
+
+    // if (v1 == 7 && v2 == 4)
+    // {
+    //     cout << "Both conditions are true" << endl;
+    // }
+    // else
+    // {
+    //     cout << "One or more condition is false" << endl;
+    // }
+
+    // if (v1 == 8 || v2 == 4)
+    // {
+    //     cout << "One or more condition is true" << endl;
+    // }
+    // else
+    // {
+    //     cout << "All conditions are false" << endl;
+    // }
+
+    // bool condition = (v2 != 8) && (v1 == 10);
+    // cout << condition << endl;
+    // bool condition2 = condition;
+    // cout << condition2 << endl;
+
+    return 0;
+}
