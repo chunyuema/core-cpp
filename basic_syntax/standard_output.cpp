385610c4c49c990b8666b2f3aff45e6d80b38a49
Chunyue Ma <cma01@wesleyan.edu>
2022-10-02 08:19:45 -0700

    cout intro

---
+++ basic_syntax/standard_output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    /*
+    object oriented, we are using cout object to print; inherently aware of the type of variables
+    */
+    std::cout << "Hello World"
+              << 2 * 7
+              << "Another String"
+              << std::endl;
+    return 0;
+}
