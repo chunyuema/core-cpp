32d5833abcf95e570d9beefc9bcb2c76cafd88cb
Chunyue Ma <cma01@wesleyan.edu>
2022-10-05 08:20:59 -0700

    access struct through ref

--- basic_syntax/structure_basics.cpp
+++ basic_syntax/structure_basics.cpp
@@ -7,12 +7,32 @@ struct MyStruct
     const char *s;
 };
 
-int main()
+void check_size_of_struct()
+{
+    MyStruct s1 = {5, 4.19, "my test string"};
+    printf("size of s1 =  %zd bits\n", sizeof(s1) * 8);
+}
+
+void access_struct_directly()
 {
     MyStruct s1 = {5, 4.19, "my test string"};
     printf("s1 = { %d, %f, %s }\n", s1.i, s1.d, s1.s);
     printf("Modifying the structure\n");
     s1.d = 5.20;
     printf("s1 = { %d, %f, %s }\n", s1.i, s1.d, s1.s);
+}
+
+void access_struct_through_pointer()
+{
+    MyStruct s1 = {5, 4.19, "my test string"};
+    MyStruct *s2 = &s1;
+    printf("s2 = { %d, %f, %s }\n", s2->i, s2->d, s2->s);
+}
+
+int main()
+{
+    check_size_of_struct();
+    // access_struct_directly();
+    // access_struct_through_pointer();
     return 0;
 }
