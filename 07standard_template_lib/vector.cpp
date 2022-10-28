cd82aee6fef29ab9a8dc409ac0fe0912b16fc531
Chunyue Ma <cma01@wesleyan.edu>
2022-10-28 08:01:06 -0700

    update print with template, add more sample code

--- 07standard_template_lib/vector.cpp
+++ 07standard_template_lib/vector.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 using namespace std;
 
-void print_vector(vector<double> values)
+template <typename T>
+void print_vector(string message, vector<T> values)
 {
+    cout << message << endl;
     for (int i = 0; i < values.size(); i++)
     {
         cout << values[i];
@@ -11,6 +13,51 @@ void print_vector(vector<double> values)
             cout << ", ";
     }
     cout << endl;
+}
+
+void insert_and_erase()
+{
+    vector<int> v;
+    for (int i = 1; i <= 5; i++)
+    {
+        v.push_back(i);
+    }
+    print_vector("initial vector", v);
+    // insert a single element
+    v.insert(v.begin() + 3, 789);
+    print_vector("insert 789", v);
+    // insert a vector
+    v.insert(v.begin(), {45, 67, 89});
+    print_vector("insert {45, 67, 89}", v);
+    // erase
+    v.erase(v.begin() + 3);
+    print_vector("erase element at index 3", v);
+}
+
+void vector_initialization()
+{
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2{4, 5, 6};
+
+    // fill vector with same values
+    vector<int> v3(10, -1);
+    vector<string> v4(5, "hi");
+    vector<string> v5(5); // five empty strings
+    vector<string> v6 = {"apple", "bear", "coke"};
+
+    // initialize from c array
+    constexpr size_t size = 10;
+    int ia[size] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> carray_v(ia, ia + size);
+    print_vector("c array initialized vector", carray_v);
+
+    // using copy constructor
+    vector<int> copy_v(carray_v);
+    print_vector("initialize vector with copy", copy_v);
+
+    // using move constructor
+    vector<int> move_v(move(copy_v));
+    print_vector("initialize vector with move", move_v);
 }
 
 void vector_basics()
@@ -21,7 +68,7 @@ void vector_basics()
     {
         values.push_back(input);
     }
-    print_vector(values);
+    // print_vector(values);
 }
 
 // does not modify the original values
@@ -32,7 +79,7 @@ void multiply1(vector<double> values, double factor)
         values[i] = values[i] * factor;
     }
     cout << "Inside the function: " << endl;
-    print_vector(values);
+    // print_vector(values);
 }
 
 void multiply2(vector<double> &values, double factor)
@@ -42,20 +89,7 @@ void multiply2(vector<double> &values, double factor)
         values[i] = values[i] * factor;
     }
     cout << "Inside the function: " << endl;
-    print_vector(values);
-}
-
-void vector_initialization()
-{
-    vector<int> v1 = {1, 2, 3};
-    vector<int> v2{4, 5, 6};
-    vector<int> v3(10, -1);
-    vector<string> v4(5, "hi");
-    vector<string> v5(5); // five empty strings
-    vector<string> v6 = {"apple", "bear", "coke"};
-    for (auto s : v6)
-        cout << s << " ";
-    cout << endl;
+    // print_vector(values);
 }
 
 void vector_comparison()
@@ -77,6 +111,8 @@ void vector_comparison()
 int main()
 {
     // vector_basics();
+    vector_initialization();
+    insert_and_erase();
 
     // vector<double> values;
     // values.push_back(1);
