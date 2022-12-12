c2056aa3b0351a658b405f602a9d0f21c1a5c4a6
Chunyue Ma <cma01@wesleyan.edu>
2022-12-12 08:30:58 -0800

    pointer to const and const pointers #1 #2

--- 01data_type/03compound_types/01pointers_basics.cpp
+++ 01data_type/03compound_types/01pointers_basics.cpp
@@ -48,6 +48,41 @@ void compare_pointers()
     cout << "p1 and p2 are " << (p1 == p2 ? "equal" : "not equal") << endl;
 }
 
+void pointers_to_const()
+{
+    const int i = 20;
+    const int *pi = &i;
+    /* pi cannot be used to modify i since it is a const pointer */
+    // *pi = 21;
+
+    int x = 20;
+    const int *px = &x;
+    /* even if x is not const, px cannot be used to modify x since it is a const pointer */
+    // *px = 21;
+
+    /* pj cannot point to i since pj is not a constant pointer, it can modify i, which should not be allowed */
+    // int *pj = &i;
+}
+
+void const_pointers()
+{
+    int x = 90;
+    /* px is a const pointer to x */
+    int *const px = &x;
+    /* py is a pointer to a const int */
+    const int *py = &x;
+    /* pz is a const pointer to a const int */
+    const int *const pz = &x;
+
+    int y = 80;
+
+    /* px cannot be reassigned since it is a const itself */
+    // px = &y;
+
+    /* py can be reassigned, it points to a const int, but is not a const itself */
+    py = &y;
+}
+
 int main()
 {
     // pointer_initialization();
