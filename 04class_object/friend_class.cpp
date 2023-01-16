eb6f90ea7cce4f2bfd0a7ebf761e6acb4609f6f4
Chunyue Ma <cma01@wesleyan.edu>
2023-01-16 10:32:26 -0800

    rename variable for clarity #12

--- 04class_object/friend_class.cpp
+++ 04class_object/friend_class.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-class Class1
+class A
 {
-    friend class Class1_manager;
+    friend class A_manager;
     int x;
 
 public:
-    Class1(int x) : x(x) {}
+    A(int x) : x(x) {}
     int get_x() { return x; }
     void set_x(int new_x) { x = new_x; }
 };
@@ -16,29 +16,29 @@ public:
 /*
 Note that class1 manager has access to the non public fields of class 1 because class 1 declared it as the friend class
 */
-class Class1_manager
+class A_manager
 {
 public:
     void display()
     {
-        cout << "{ " << c1.x << " }" << endl;
+        cout << "{ " << a.x << " }" << endl;
     }
 
     void clear()
     {
-        c1.x = 0;
+        a.x = 0;
     }
 
 private:
     // class 1 manager manage this c1 object
-    Class1 c1{1000000};
+    A a{1000000};
 };
 
 int main()
 {
-    Class1_manager cm;
-    cm.display();
-    cm.clear();
-    cm.display();
+    A_manager m;
+    m.display();
+    m.clear();
+    m.display();
     return 0;
 }
