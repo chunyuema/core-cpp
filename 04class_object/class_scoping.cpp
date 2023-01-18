8ebdeaf344957ccc8e787df4badfea081b4387e8
Chunyue Ma <cma01@wesleyan.edu>
2023-01-18 08:51:25 -0800

    name lookup and class scope #13

---
+++ 04class_object/class_scoping.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* this msg will not be used by the class S */
+string msg = "chunyue from the outside world";
+
+class S
+{
+private:
+    string msg = "chunyue from the inside world";
+
+public:
+    void
+    dummy(string msg)
+    {
+        /* this msg will refer to the parameter */
+        cout << msg << endl;
+    }
+
+    void dummy2()
+    {
+        /* this msg will refer to the data member msg */
+        cout << msg << endl;
+    }
+};
+
+int main()
+{
+    S s;
+    s.dummy("chunyue from the function world");
+    s.dummy2();
+    return 0;
+}
