34a0eb6e1d7e27330ce6fd4f41572b49c1c9f8c0
Chunyue Ma <cma01@wesleyan.edu>
2023-01-21 12:12:18 -0800

    implicit class type conversion with constructor #9

---
+++ 04class_object/constructor_destructor/conversion_constructor.cpp
@@ -0,0 +1,37 @@
+/*
+Modified from https://learn.microsoft.com/en-us/cpp/cpp/user-defined-type-conversions-cpp?view=msvc-170#ConvCTOR
+*/
+
+#include <iostream>
+using namespace std;
+
+class Money
+{
+
+public:
+    Money() : amount{0.0} {};
+    Money(double _amount) : amount{_amount} {};
+    // Mark the method const so that it can be called by const objects
+    double get_amount() const { return amount; }
+
+private:
+    double amount;
+};
+
+void display_balance(const Money balance)
+{
+    std::cout << "The balance is: " << balance.get_amount() << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    // Create a Money object and then call the method
+    Money payable{79.99};
+    display_balance(payable);
+
+    // Directly converting double into Money objects
+    display_balance(49.95);
+    display_balance(9.99f);
+
+    return 0;
+}
