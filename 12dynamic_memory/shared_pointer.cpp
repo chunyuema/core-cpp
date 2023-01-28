ac57d485a67e5b5a944bcafd783631bad908c8a9
Chunyue Ma <cma01@wesleyan.edu>
2023-01-28 12:11:16 -0800

    clean up code

--- 12dynamic_memory/shared_pointer.cpp
+++ 12dynamic_memory/shared_pointer.cpp
@@ -7,29 +7,18 @@ int main()
 {
     shared_ptr<string> p1 = make_shared<string>(5, 'c');
     if (p1)
-    {
         cout << "p1: { " << p1 << ": " << *p1 << " }" << endl;
-    }
-
     if (p1.unique())
-    {
         cout << "Unique pointer" << endl;
-    }
     else
-    {
         cout << "Non-unique pointer: " << p1.use_count() << endl;
-    }
 
     // p2 is also pointing to what p1 is pointing at
     auto p2(p1);
 
     if (p1.unique())
-    {
         cout << "Unique pointer" << endl;
-    }
     else
-    {
         cout << "Non-unique pointer: " << p1.use_count() << endl;
-    }
     return 0;
 }
