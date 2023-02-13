bec990694b45eebdeffc8867ac6376337a3e4eab
Chunyue Ma <cma01@wesleyan.edu>
2023-02-13 08:30:55 -0800

    add StrVec as integrated example of copy control [tbc] #15

---
+++ copy_control/string_vector_example.h
@@ -0,0 +1,45 @@
+#include <memory.h>
+#include <string>
+
+class StrVec
+{
+public:
+    StrVec() : elements(nullptr), first_free(nullptr), cap(nullptr) {}
+
+    StrVec(const StrVec &); // copy constructor
+
+    StrVec &operator=(const StrVec &);
+
+    ~StrVec();
+
+    void push_back(const std::string &);
+
+    size_t size() const { return first_free - elements; }
+
+    size_t capacity() const { return cap - elements; }
+
+    std::string *begin() const { return elements; }
+
+    std::string *end() const { return first_free; }
+
+private:
+    static std::allocator<std::string> alloc;
+
+    void check_and_alloc()
+    {
+        if (size() == capacity())
+            reallocate();
+    }
+
+    std::pair<std::string *, std::string *> alloc_and_copy(const std::string *, const std::string *);
+
+    void free();
+
+    void reallocate();
+
+    std::string *elements;
+    std::string *first_free;
+    std::string *cap;
+};
+
+std::allocator<std::string> StrVec::alloc;
