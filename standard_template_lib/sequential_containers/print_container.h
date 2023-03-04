090715ee1c76bdee9b5598e96b4fca8745ec7af6
Chunyue Ma <cma01@wesleyan.edu>
2023-03-04 08:59:05 -0800

    add print list with iterator #15

--- standard_template_lib/sequential_containers/print_container.h
+++ standard_template_lib/sequential_containers/print_container.h
@@ -1,12 +1,13 @@
 #include <string>
 #include <vector>
+#include <list>
 
 using namespace std;
 
 template <typename T>
 void print_vector(std::string message, std::vector<T> &values)
 {
-    cout << message << "[ ";
+    cout << message << " vector = [ ";
     for (int i = 0; i < values.size(); i++)
     {
         cout << values[i];
@@ -17,9 +18,24 @@ void print_vector(std::string message, std::vector<T> &values)
 }
 
 template <typename T>
+void print_list_with_iterator(std::string message, list<T> &values)
+{
+    cout << message << " list = [ ";
+    auto it = values.begin();
+    while (it != values.end())
+    {
+        cout << *it;
+        it++;
+        if (it != values.end())
+            cout << ", ";
+    }
+    cout << " ]" << endl;
+}
+
+template <typename T>
 void print_vector_with_iterator(string message, vector<T> &values)
 {
-    cout << message << "[ ";
+    cout << message << " vector = [ ";
     auto it = values.begin();
     while (it != values.end())
     {
