6c3930c0c453bfa48ec9d5255a8c2ae1ea692b0f
Chunyue Ma <cma01@wesleyan.edu>
2023-03-04 08:58:51 -0800

    update initialization with another container #15

--- standard_template_lib/sequential_containers/container_initialization.cpp
+++ standard_template_lib/sequential_containers/container_initialization.cpp
@@ -16,22 +16,43 @@ void size_related_constructor()
 
 void initialize_with_another_container()
 {
-    list<const char *> lnames = {"chunyue", "bobby", "rob"};
-    vector<string> vnames(lnames.begin(), lnames.end());
+    // Direct initialization with another container
+    list<string> l1 = {"chunyue", "bobby", "rob"};
+    list<string> l2(l1);
+
+    // Initialization with container denoted with decorator
+    list<string> l3 = {"chunyue", "bobby", "rob"};
+    vector<string> v1(l3.begin(), l3.end());
+    auto it1 = l3.begin();
+    auto it2 = l3.begin();
+    it2++;
+    vector<string> v2(it1, it2);
+    it2++;
+    vector<string> v3(it1, it2);
+    print_vector("v1", v1);
+    print_vector("v2", v2);
+    print_vector("v3", v3);
 
     // The following line does not work due to incompatibility of type
     // vector<string> vnames2(lnames);
 
     // The following line does not work either
     // vector<const char *> vnames3(lnames);
+}
 
+void list_initialization()
+{
+    list<std::string> lnames = {"chunyue", "bobby", "rob"};
+    print_list_with_iterator("lnames", lnames);
+
+    vector<const char *> vnames = {"chunyue", "bobby", "rob"};
     print_vector("vnames", vnames);
-    print_vector_with_iterator("vnames", vnames);
 }
 
 int main()
 {
     // size_related_constructor();
     initialize_with_another_container();
+    // list_initialization();
     return 0;
 }
