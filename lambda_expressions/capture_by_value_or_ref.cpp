1ffafa5f1627181d92998e3f405530a4cade8d20
Chunyue Ma <cma01@wesleyan.edu>
2023-03-19 09:04:57 -0700

    Mutable lambda #21

--- lambda_expressions/capture_by_value_or_ref.cpp
+++ lambda_expressions/capture_by_value_or_ref.cpp
@@ -14,15 +14,29 @@ void capture_by_value()
     cout << f() << endl;
 }
 
+void capture_by_mutable_value()
+{
+    int v1 = 10000;
+    // with mutable, you can change the value of the captured value
+    auto f = [v1]() mutable
+    { return ++v1; };
+    cout << f() << endl;
+    // original v1 is not affected
+    cout << v1 << endl;
+}
+
 void capture_by_reference()
 {
     int v1 = 10000;
+    // no mutable keyword is required as v1 is captured by reference
     auto f = [&v1]
-    { return v1; };
+    { return ++v1; };
     // reassign v1
     v1 = 10;
     // affects the lambda expression
     cout << f() << endl;
+    // original v1 is updated to 11 as well
+    cout << v1 << endl;
 }
 
 void capture_by_reference_necessary(vector<string> &words, ostream &os = cout, char c = ' ')
@@ -37,10 +51,9 @@ int main()
 {
     capture_by_value();
     capture_by_reference();
+    capture_by_mutable_value();
 
-    vector<string> str_vec = {"hello!",
-                              "hi~",
-                              "how are you?"};
+    vector<string> str_vec = {"hello!", "hi~", "how are you?"};
     capture_by_reference_necessary(str_vec);
     return 0;
 }
