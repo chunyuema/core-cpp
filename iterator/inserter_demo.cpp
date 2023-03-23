86d9ebda5eb71b434198b71e27b28a21c8f2c722
Chunyue Ma <cma01@wesleyan.edu>
2023-03-22 21:01:19 -0700

    Update with do nothing ops on inserter #21

--- iterator/inserter_demo.cpp
+++ iterator/inserter_demo.cpp
@@ -20,7 +20,7 @@ void inserter_vs_insert()
     // getting the inserter for insertion
     auto it2 = inserter(l2, l2.begin());
     // insert at the inserter iterator
-    *it2 = 4;
+    it2 = 4;
     print_list_with_iterator("l2", l2);
 }
 
@@ -30,7 +30,7 @@ void front_inserter_iterator()
     auto fi = front_inserter(l1);
 
     // insert a number to v1 using fi
-    *fi = 6;
+    fi = 6;
     print_list_with_iterator("insert 6 at the front", l1);
 
     list<int> l2 = {99, 100};
@@ -44,9 +44,33 @@ void front_inserter_iterator()
     print_list_with_iterator("insert vector v2 at the front", l1);
 }
 
+void futile_ops()
+{
+    /*
+     *it, it++, ++it all return it and do nothing if it is an inserter
+     */
+    list<int> l = {1, 2, 3};
+    // getting the inserter for insertion
+    auto it = inserter(l, l.begin());
+    // insert at the inserter iterator
+    *it = 4;
+    print_list_with_iterator("l", l);
+
+    l = {1, 2, 3};
+    it = inserter(l, l.begin());
+    ++it = 4;
+    print_list_with_iterator("l", l);
+
+    l = {1, 2, 3};
+    it = inserter(l, l.begin());
+    it++ = 4;
+    print_list_with_iterator("l", l);
+}
+
 int main()
 {
-    // inserter_vs_insert();
+    inserter_vs_insert();
     front_inserter_iterator();
+    futile_ops();
     return 0;
 }
