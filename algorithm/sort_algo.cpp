f9ef72fa2c135208c77cb5b76225fc28496286d7
Chunyue Ma <cma01@wesleyan.edu>
2023-03-26 11:33:06 -0700

    Update demo function for generic sort #21

--- algorithm/sort_algo.cpp
+++ algorithm/sort_algo.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <deque>
 #include "../print_containers.h"
 
 using namespace std;
@@ -16,12 +17,51 @@ void print_str_vector(vector<string> &str_vec)
     cout << "]" << endl;
 }
 
-void use_sort()
+/* Generic Sort Algorithm supports the following containers: Vector, C array, Array, Deque, String */
+void generic_sort()
 {
+    /* sort vector */
     vector<string> fruits = {"pear", "apple", "grape", "peach"};
-    print_str_vector(fruits);
+    print_vector("Fruits", fruits);
     sort(fruits.begin(), fruits.end());
-    print_str_vector(fruits);
+    print_vector("Fruits", fruits);
+
+    /* sort c array */
+    int myints[] = {32, 71, 12, 45, 26, 80, 53, 33};
+    int size = sizeof(myints) / sizeof(myints[0]);
+    sort(myints, myints + size);
+    for (int i = 0; i < size; ++i)
+        cout << myints[i] << " ";
+    cout << endl;
+
+    /* sort array */
+    array<int, 8> myints2 = {32, 71, 12, 45, 26, 80, 53, 33};
+    // myints2.begin() + 8 is allowed because the iterator is random access iterator
+    sort(myints2.begin(), myints2.begin() + 8);
+    for (int i = 0; i < size; ++i)
+        cout << myints2[i] << " ";
+    cout << endl;
+
+    /* sort deque */
+    deque<string> fruits2 = {"pear", "apple", "grape", "peach"};
+    sort(fruits2.begin(), fruits2.end());
+    print_deque("Fruits", fruits2);
+
+    /* sort string */
+    string name = "ChunyueMa";
+    sort(name.begin(), name.end());
+    cout << name << endl;
+}
+
+void partial_sort()
+{
+    // code from https://cplusplus.com/reference/algorithm/sort/
+    int myints[] = {32, 71, 12, 45, 26, 80, 53, 33};
+    vector<int> myvector(myints, myints + 8); // 32 71 12 45 26 80 53 33
+
+    // myvector.begin() + 4 is allowed because the iterator here is a random access iterator
+    sort(myvector.begin(), myvector.begin() + 4); //(12 32 45 71)26 80 53 33
+    print_vector("myvector", myvector);
 }
 
 bool is_shorter(const string &s1, const string &s2)
@@ -37,39 +77,8 @@ void customize_sort()
     print_str_vector(fruits);
 }
 
-void partial_sort()
-{
-    // code from https://cplusplus.com/reference/algorithm/sort/
-    int myints[] = {32, 71, 12, 45, 26, 80, 53, 33};
-    vector<int> myvector(myints, myints + 8); // 32 71 12 45 26 80 53 33
-
-    // myvector.begin() + 4 is allowed because the iterator here is a random access iterator
-    sort(myvector.begin(), myvector.begin() + 4); //(12 32 45 71)26 80 53 33
-    print_vector("myvector", myvector);
-}
-
-void sort_arrays()
-{
-    int myints[] = {32, 71, 12, 45, 26, 80, 53, 33};
-    int size = sizeof(myints) / sizeof(myints[0]);
-    sort(myints, myints + size);
-    for (int i = 0; i < size; ++i)
-        cout << myints[i] << " ";
-    cout << endl;
-
-    array<int, 8> myints2 = {32, 71, 12, 45, 26, 80, 53, 33};
-    // myints2.begin() + 8 is allowed because the iterator is random access iterator
-    sort(myints2.begin(), myints2.begin() + 8);
-    for (int i = 0; i < size; ++i)
-        cout << myints2[i] << " ";
-    cout << endl;
-}
-
 int main()
 {
-    // use_sort();
-    // customize_sort();
-    // partial_sort();
-    sort_arrays();
+    generic_sort();
     return 0;
 }
--- print_containers.h
+++ print_containers.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <deque>
 
 using namespace std;
 
@@ -8,6 +9,19 @@ template <typename T>
 void print_vector(std::string message, std::vector<T> &values)
 {
     cout << message << " vector = [ ";
+    for (int i = 0; i < values.size(); i++)
+    {
+        cout << values[i];
+        if (i != values.size() - 1)
+            cout << ", ";
+    }
+    cout << " ]" << endl;
+}
+
+template <typename T>
+void print_deque(std::string message, std::deque<T> &values)
+{
+    cout << message << " deque = [ ";
     for (int i = 0; i < values.size(); i++)
     {
         cout << values[i];
